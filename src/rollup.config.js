d8e1e87de4c4c1ad3b68b35014618bc569652ee9
Ivan da Silveira <ivandasilveira@gmail.com>
2019-08-12 17:19:48 +0200

    changes

--- src/rollup.config.js
+++ src/rollup.config.js
@@ -3,12 +3,19 @@ const resolve		= require('rollup-plugin-node-resolve');
 const { uglify }	= require("rollup-plugin-uglify");
 const coffeescript	= require("rollup-plugin-coffee-script");
 const commonjs		= require("rollup-plugin-commonjs");
+const ignore 		= require('rollup-plugin-ignore');
+const json			= require('rollup-plugin-json');
 
 module.exports = {
 	plugins: [
-		resolve(),
+		resolve({
+			preferBuiltins: true,
+			extensions: ['.js', '.coffee', '.json'],
+		}),
+		ignore(['aws-sdk']),
+		json(),
 		coffeescript(),
-		commonjs(),
-        uglify(),
+		commonjs({ extensions: ['.js', '.coffee'] }),
+        // uglify(),
 	]
 }
